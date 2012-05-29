c4cf17bc236d062180aecf11c5cbcbec9e6ebf67
dm4 <sunrisedm4@gmail.com>
2012-05-29 13:51:58 +0800

    show cross at center

--- FadeoutAnimation.cpp
+++ FadeoutAnimation.cpp
@@ -28,11 +28,15 @@ void FadeoutAnimation::play(Picture &picture) {
 		}
 
 		if(picture.getIsFocus()){
+            int len = 60;
+            int centerX = picture.cols / 2;
+            int centerY = picture.rows / 2;
+
 			CvPoint p1, p2, p3, p4;
-			p1 = cvPoint(58, 59);
-			p2 = cvPoint(58, 119);
-			p3 = cvPoint(30, 88);
-			p4 = cvPoint(90, 88);
+            p1 = cvPoint(centerX, centerY - len / 2);
+            p2 = cvPoint(centerX, centerY + len / 2);
+            p3 = cvPoint(centerX - len / 2, centerY);
+            p4 = cvPoint(centerX + len / 2, centerY);
 
 			CvScalar color;
 			color = CV_RGB(255, 0 ,0);
